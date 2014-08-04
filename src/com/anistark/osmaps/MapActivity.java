73f6e5c3012f0e5a723471bcfa9a76fd32dfa67c
Kumar Anirudha <anirudhastark@yahoo.com>
2014-08-04 15:28:05 +0530

    Migrated

---
+++ src/com/anistark/osmaps/MapActivity.java
@@ -0,0 +1,50 @@
+package com.anistark.osmaps;
+
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.SimpleLocationOverlay;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MapActivity extends Activity {
+    private MapView mapView;
+    private MapController mapController;
+    private ScaleBarOverlay mScaleBarOverlay;
+    private SimpleLocationOverlay mMyLocationOverlay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        mapView = (MapView) this.findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+        mapView.setMultiTouchControls(true);
+        mapController = (MapController) this.mapView.getController();
+        mapController.setZoom(2);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.map, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
